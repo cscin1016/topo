8666efa3b1f79ad17b11c57aa9c1a0c1cc807d1d
CSC_Project <915893620@qq.com>
2016-10-08 15:00:17 +0800

    颜总的一次外包构想
    开始前的demo，自动布局实现六块视图

---
+++ topo/topo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  topo
+//
+//  Created by 陈双超 on 15/1/12.
+//  Copyright (c) 2015年 陈双超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ topo/topo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  topo
+//
+//  Created by 陈双超 on 15/1/12.
+//  Copyright (c) 2015年 陈双超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
